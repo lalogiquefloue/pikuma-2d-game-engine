85d9e53de64e6ddcb41013abb14f13c5762d3c68
Carl Thibault <lalogiquefloue@gmail.com>
2025-11-10 14:44:06 -0500

    add basic ECS folder structure

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include "../Logger/Logger.h"
+#include "../ECS/ECS.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <glm/glm.hpp>
@@ -69,12 +70,12 @@ void Game::ProcessInput() {
 	}
 }
 
-glm::vec2 playerPosition;
-glm::vec2 playerVelocity;
-
 void Game::Setup() {
-	playerPosition = glm::vec2(10.0, 20.0);
-	playerVelocity = glm::vec2(50.0, 25.0);
+	// TODO:
+	// Entity tank = registry.CreateEntity();
+	// tank.addComponent<TransformComponent>
+	// tank.addComponent<BoxColliderComponent>
+	// tank.addComponent<SpriteComponent>("./assets/images/tank.png")
 }
 
 void Game::Update() {
@@ -90,27 +91,17 @@ void Game::Update() {
 	// store previous frame time
 	millisecsPreviousFrame = SDL_GetTicks();
 
-	playerPosition.x += playerVelocity.x * deltaTime;
-	playerPosition.y += playerVelocity.y * deltaTime;
+	// TODO:
+	// MovementSystem.Update();
+	// CollisionSystem.Update();
+	// DamageSystem.Update();
 }
 
 void Game::Render() {
 	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
 	SDL_RenderClear(renderer);
 
-	// Draw a PNG texture
-	SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_FreeSurface(surface);
-
-	SDL_Rect dstRect = { 
-		static_cast<int>(playerPosition.x), 
-		static_cast<int>(playerPosition.y),
-		32, 
-		32 
-	};
-	SDL_RenderCopy(renderer, texture, NULL, &dstRect);
-	SDL_DestroyTexture(texture);
+	// TODO: Render game objects
 
 	SDL_RenderPresent(renderer);
 }

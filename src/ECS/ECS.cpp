865cd3f98fe485197341310001cb5f037002a10b
Carl Thibault <lalogiquefloue@gmail.com>
2025-11-11 10:25:06 -0500

    implement system functions

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,3 +1,27 @@
 #include "ECS.h"
 
-// TODO: ECS.h implementations
+int Entity::GetId() const {
+    return id;
+}
+
+void System::AddEntityToSystem(Entity entity){
+    entities.push_back(entity);
+};
+
+void System::RemoveEntityFromSystem(Entity entity){
+    entities.erase(std::remove_if(
+        entities.begin(), 
+        entities.end(), 
+        [&entity](Entity other){
+            return entity.GetId() == other.GetId();
+        }
+    ));
+};
+
+std::vector<Entity> System::GetSystemEntities() const{
+    return entities;
+};
+
+const Signature& System::GetComponentSignature() const{
+    return componentSignature;
+};
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,20 +1,68 @@
 #ifndef ECS_H
 #define ECS_H
 
-class Component {
-    // TODO
+#include <bitset>
+#include <vector>
+#include <algorithm> 
+
+const unsigned int MAX_COMPONENTS = 32;
+
+// We use a bitset to keep track of which components an entity has
+// and also helsp keep track of which entities a system is interested in.
+typedef std::bitset<MAX_COMPONENTS> Signature;
+#include <algorithm> 
+struct IComponent {
+    protected:
+        static int nextId;
+};
+
+// Used to assign a unique id to a component type
+template <typename T>
+class Component: IComponent {
+    // Returns the unique id of Component<T>
+    static int GetId(){
+        static auto id = nextId++;
+        return id;
+    }
 };
 
 class Entity {
-    // TODO
+    private:
+        int id;
+
+    public:
+        Entity(int id): id(id) {};
+        int GetId() const;
+        //...
 };
 
+// The system processes entities that contain a specific signature
 class System {
-    // TODO
+    private: 
+        Signature componentSignature;
+        std::vector<Entity> entities;
+    
+    public:
+        System() = default;
+        ~System() = default;
+
+        void AddEntityToSystem(Entity entity);
+        void RemoveEntityFromSystem(Entity entity);
+        std::vector<Entity> GetSystemEntities() const;
+        const Signature& GetComponentSignature() const;
+
+        // Defines the component type that entities must have to be considered by the system
+        template <typename TComponent> void RequireComponent();
 };
 
 class Registry {
     // TODO
+};
+
+template <typename TComponent>
+void System::RequireComponent() {
+    const auto componentId = Component<TComponent>::GetId();
+    componentSignature.set(componentId);
 };
 
 #endif

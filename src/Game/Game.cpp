664762d151e6629c001bfa01d59df7b918799dd3
Carl Thibault <lalogiquefloue@gmail.com>
2026-01-27 15:35:09 -0500

    implement the render system

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -3,7 +3,9 @@
 #include "../ECS/ECS.h"
 #include "../Components/TransformComponent.h"
 #include "../Components/RigidBodyComponent.h"
+#include "../Components/SpriteComponent.h"
 #include "../Systems/MovementSystem.h"
+#include "../Systems/RenderSystem.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <glm/glm.hpp>
@@ -73,14 +75,19 @@ void Game::ProcessInput() {
 void Game::Setup() {
 	// Add the systems that need to be processed in our game
 	registry->AddSystem<MovementSystem>();
+	registry->AddSystem<RenderSystem>();
 
-	// Create some entity
+	// Create some entity and add some components to that entity
 	Entity tank = registry->CreateEntity();
-
-	// Add some components to that entity
 	tank.AddComponent<TransformComponent>(glm::vec2(10.0, 30.0), glm::vec2(1.0, 1.0), 0.0);
-	tank.AddComponent<RigidBodyComponent>(glm::vec2(1.0, 1.0));
-	// tank.RemoveComponent<TransformComponent>(); // test
+	tank.AddComponent<RigidBodyComponent>(glm::vec2(10.0, 10.0));
+	tank.AddComponent<SpriteComponent>(10, 10);
+
+	// Create some entity and add some components to that entity
+	Entity truck = registry->CreateEntity();
+	truck.AddComponent<TransformComponent>(glm::vec2(30.0, 60.0), glm::vec2(1.0, 1.0), 0.0);
+	truck.AddComponent<RigidBodyComponent>(glm::vec2(10.0, 50.0));
+	truck.AddComponent<SpriteComponent>(15, 30);
 }
 
 void Game::Update() {
@@ -108,7 +115,8 @@ void Game::Render() {
 	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
 	SDL_RenderClear(renderer);
 
-	// TODO: Render game objects
+	// invoke all the systems thst need to render
+	registry->GetSystem<RenderSystem>().Update(renderer);
 
 	SDL_RenderPresent(renderer);
 }
